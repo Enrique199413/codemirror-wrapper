c32f52466d8fcea276f188d699649a2379217039
Sandra Díaz <sandra.diaz@gft.com>
2019-06-12 12:11:05 -0500

    #2 configure dependencies

--- codemirror-wrapper.js
+++ codemirror-wrapper.js
@@ -11,22 +11,52 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 class CodemirrorWrapper extends PolymerElement {
   static get template() {
     return html`
+      <link href="../node_modules/codemirror/lib/codemirror.css" rel="stylesheet">
+      <link href="../node_modules/codemirror/theme/night.css" rel="stylesheet">
       <style>
         :host {
           display: block;
         }
       </style>
       <h2>Hello [[prop1]]!</h2>
+      <textarea id="textArea"></textarea>
+      <div id="code"></div>
     `;
   }
   static get properties() {
     return {
-      prop1: {
+      value: {
         type: String,
-        value: 'codemirror-wrapper',
+        value: 'test-polytres',
       },
+      valorCorrecto: {
+        type: String,
+        value: 'valorCorrecto;',
+        reflectToAttribute: true,
+        observer: '_changeValueOnTextArea'
+      }
     };
   }
+
+  _changeValueOnTextArea(newValue) {
+    setTimeout(() => {
+      this.editor.setValue(newValue);
+
+    }, 0);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+      let options = {
+        mode:  "javascript",
+        lineNumbers: 'true',
+        theme: 'night'
+      };
+      //CodeMirror(this.shadowRoot.querySelector('#textArea'), options);
+      this.set('editor', CodeMirror.fromTextArea(this.shadowRoot.querySelector('#textArea'), options));
+    
+  }
+  
 }
 
 window.customElements.define('codemirror-wrapper', CodemirrorWrapper);
